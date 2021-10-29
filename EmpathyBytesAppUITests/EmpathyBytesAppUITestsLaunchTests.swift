3c978617024979ec924119dbd52b8063822e7fa1
hkaranth3 <hkaranth3@gatech.edu>
2021-10-29 16:02:20 -0400

    Initial Commit

---
+++ EmpathyBytesAppUITests/EmpathyBytesAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EmpathyBytesAppUITestsLaunchTests.swift
+//  EmpathyBytesAppUITests
+//
+//  Created by Harsha Karanth on 10/29/21.
+//
+
+import XCTest
+
+class EmpathyBytesAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
