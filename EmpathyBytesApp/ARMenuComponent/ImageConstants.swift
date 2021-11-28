cd34429a8f71180609404bb88b8bfc5467308467
nive1616 <nchellam3@gatech.edu>
2021-11-28 13:08:07 -0500

    Adding dummy

--- EmpathyBytesApp/ARMenuComponent/ImageConstants.swift
+++ EmpathyBytesApp/ARMenuComponent/ImageConstants.swift
@@ -11,10 +11,13 @@ import UIKit
 struct ImageConstants {
     static let toyAirplane = "ToyAirplane";
     static let guitar      = "Guitar";
+    static let drumTable = "drumTable";
     static let toyAirplaneTitle : String = "My childhood toy"
     static let guitarTitle : String = "My guitar gently weeps"
+    static let drumTableTitle = "Furniture by Matthew"
     static let toyAirplaneDescription:String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     static let guitarAirplaneDescription:String = "Diam vulputate ut pharetra sit amet aliquam id. Volutpat diam ut venenatis tellus in metus vulputate eu. Aliquam id diam maecenas ultricies mi eget. Vivamus at augue eget arcu dictum varius. Scelerisque varius morbi enim nunc. Nisi scelerisque eu ultrices vitae auctor eu. Ac turpis egestas integer eget. Nunc pulvinar sapien et ligula. Venenatis urna cursus eget nunc scelerisque viverra. Rhoncus aenean vel elit scelerisque mauris pellentesque."
+    static let drumTableDescription:String = "Matthew used the Makerspace to create different furniture pieces to liven up and transform his room. He used the Hive Makerspace to stain and cut pieces of wood that he later used in building his furniture. Additionally, he used the machinery in the makerspace and his skills in wood-building to create floats for the Homecoming parade."
     
 
 }
